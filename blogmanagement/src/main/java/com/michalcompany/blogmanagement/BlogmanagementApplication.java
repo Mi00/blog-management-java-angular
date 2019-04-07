e669849a10d7e4b47fb86aaf609b507d0a245b90
Michał <michal@hand2band.media>
2019-04-07 15:18:35 +0200

    added blog class with jpa auditing

--- blogmanagement/src/main/java/com/michalcompany/blogmanagement/BlogmanagementApplication.java
+++ blogmanagement/src/main/java/com/michalcompany/blogmanagement/BlogmanagementApplication.java
@@ -2,8 +2,10 @@ package com.michalcompany.blogmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BlogmanagementApplication {
 
 	public static void main(String[] args) {
---
+++ blogmanagement/src/main/java/com/michalcompany/blogmanagement/model/Blog.java
@@ -0,0 +1,92 @@
+package com.michalcompany.blogmanagement.model;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "blogs")
+@EntityListeners(AuditingEntityListener.class)
+public class Blog {
+
+    private long id;
+    private String title;
+    private String body;
+    private Date createdAt;
+    private Date updatedAt;
+    private String createdBy;
+    private String updatedBy;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "title", nullable = false)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(name = "body", nullable = false)
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Column(name = "created_at", nullable = false)
+    @CreatedDate
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Column(name = "updated_at", nullable = false)
+    @LastModifiedDate
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Column(name = "created_by", nullable = false)
+    @CreatedBy
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    @Column(name = "updated_by", nullable = false)
+    @LastModifiedBy
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+}
